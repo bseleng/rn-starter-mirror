0284ed84d17f186d56377d974c57fa7a7b64ab1c
Bogdan S <bseleng@gmail.com>
2022-02-28 23:14:16 +0300

    SEC-2 - feat: add interactive username

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,14 +1,71 @@
-import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import {Button, Pressable, StyleSheet, Text, TextInput, View} from 'react-native';
 
 const ComponentsScreen = () => {
-  return <Text style={styles.text}>Components Screen</Text>;
+  const [userName, setUserName] = useState('');
+  const [isEditable, setIsEditable] = useState(true);
+  const onChange = ({nativeEvent: {eventCount, target, text}}) => {
+    setUserName(text)
+  }
+  const toggleEditable = () => {
+    setIsEditable(!isEditable)
+  }
+
+  return (
+    <View style={styles.wrap}>
+      <Text style={styles.title}>Getting started with React Native</Text>
+      {isEditable && (
+        <View style={styles.inputWithButton}>
+          <TextInput
+            type="text"
+            style={styles.input}
+            placeholder="Enter your name"
+            value={userName}
+            onChange={onChange}
+          />
+          <Button
+            onPress={toggleEditable}
+            title="OK"
+            style={styles.button}
+          />
+        </View>
+
+
+      )}
+      <Pressable onPress={toggleEditable}>
+        <Text style={styles.text}>My name is {userName}</Text>
+      </Pressable>
+
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 30,
+  wrap: {
+    marginHorizontal: 10,
+    flex: 1,
   },
+  title: {
+    fontSize: 45,
+  },
+  text: {
+    fontSize: 20,
+  },
+  input: {
+    borderColor: 'black',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    padding: 10,
+    flex: 2,
+  },
+  button: {
+    flex: 1,
+    padding: 3,
+
+  },
+  inputWithButton: {
+    flexDirection: 'row',
+  }
 });
 
 export default ComponentsScreen;
