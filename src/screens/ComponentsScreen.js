e5db670a81e8810e0d8283722e3e8533c95c49ae
Bogdan S <bseleng@gmail.com>
2022-03-01 20:32:07 +0300

    SEC-2 - fix: add whitespace to X button
    improve button aesthetics, make it more squarelike

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -35,7 +35,7 @@ const ComponentsScreen = () => {
             />
             <Button
               onPress={() => setUserName('')}
-              title="X"
+              title="  X  "
               color={'#ce0000'}
             />
           </>
