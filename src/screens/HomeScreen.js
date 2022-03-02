d1ad21686217f6f1244820d8c0ca01098c195df5
Bogdan S <bseleng@gmail.com>
2022-03-02 20:51:29 +0300

    SEC-4 - feat: add navigation; see details below
    add Button and TouchableOpacity

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,50 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import {Text, StyleSheet, View, Button, TouchableOpacity, Pressable} from 'react-native';
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hi, it's me!</Text>;
+const HomeScreen = ({navigation: {navigate}}) => {
+  return (
+    <View>
+      <Text style={styles.text}>Hi, it's me!</Text>
+      <Button
+        title={'Components'}
+        onPress={() => navigate('Components')}
+      />
+      <TouchableOpacity
+        style={styles.customButton}
+        onPress={() => navigate('Lists')}
+      >
+        <Text
+          style={styles.customButtonText}
+        >
+          Lists
+        </Text>
+      </TouchableOpacity>
+    </View>
+
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
+    marginBottom: 5,
   },
+
+  customButton: {
+    padding: 10,
+    backgroundColor: '#3b7707',
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 5,
+
+  },
+
+  customButtonText: {
+    textAlign: 'center',
+    color: '#fff',
+    textTransform: 'uppercase',
+  }
 });
 
 export default HomeScreen;
