d017f5d5a55b57dc3fbe928bb83d68754db77574
Bogdan S <bseleng@gmail.com>
2022-03-04 00:24:31 +0300

    SEC-5 - feat: add ImageDetail layout

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -3,18 +3,32 @@ import {View, Image, Text, StyleSheet} from 'react-native'
 
 const ImageDetail = ({title, img, score}) => (
   <View style={styles.wrap}>
-    <Image source={img}/>
-    <Text> {title}</Text>
-    <Text> {score}</Text>
+    <View style={styles.container}>
+      <View>
+        <Image source={img}/>
+        <Text> {title}</Text>
+      </View>
+      <Text style={styles.score}> {score}</Text>
+    </View>
   </View>
 )
 
 const styles = StyleSheet.create({
-  wrap: {
+  container: {
     flexDirection: 'row',
-
+  },
+  score: {
+    alignSelf: 'center',
+    justifyContent: 'center',
+    fontSize: 18,
+    backgroundColor: '#494949',
+    color: '#fff',
+    borderRadius: 70,
+    marginLeft: 20,
+    textAlign: 'center',
+    padding: 20,
   },
 
 })
 
-export  default  ImageDetail
+export default ImageDetail
