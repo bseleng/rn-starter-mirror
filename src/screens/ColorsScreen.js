f51bc41529eda65761f21fa6f9d5a4a49c7bcde3
Bogdan S <bseleng@gmail.com>
2022-03-09 21:06:24 +0300

    SEC-6 - refactor: move methods outside the component

--- src/screens/ColorsScreen.js
+++ src/screens/ColorsScreen.js
@@ -1,55 +1,43 @@
 import React, {useCallback, useState} from 'react';
 import {Button, FlatList, StyleSheet, Text, View} from 'react-native';
 
+const getRandomColorValue = () => String(Math.floor(Math.random() * 256))
+const getRandomCssColor = () => `rgb(${getRandomColorValue()},${getRandomColorValue()},${getRandomColorValue()})`
 
 const ColorsScreen = () => {
   const [colors, setColors] = useState([]);
-  const randomIntRgb = () => String(Math.floor(Math.random() * 256))
   const addColor = useCallback(() => {
-    setColors(colors => [...colors, 'rgb(' + randomIntRgb() + ', ' + randomIntRgb() + ', ' + randomIntRgb() + ')'])
+    setColors(colors => [...colors, getRandomCssColor()])
   }, [colors, setColors])
 
-  return (
-    <View style={styles.wrap}>
+  return (<View style={styles.wrap}>
       <Button
         title={'Add color'}
         onPress={addColor}
       />
-      {colors.length > 0 && (
-        <FlatList
+      {colors.length > 0 && (<FlatList
           data={colors}
           keyExtractor={(item, i) => i + item}
           renderItem={({item}) => {
-            return (
-              <View style={styles.colorWrap}>
+            return (<View style={styles.colorWrap}>
                 <View style={[styles.color, styles.bgColor(item)]}>
                 </View>
                 <Text>{item}</Text>
 
-              </View>
-            )
+              </View>)
           }}
-        />
-      )}
-    </View>
-  )
+        />)}
+    </View>)
 }
 
 const styles = StyleSheet.create({
   wrap: {
-    padding: 16,
-    paddingBottom: 50,
-  },
-  colorWrap: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  color: {
-    width: 50,
-    height: 50,
-    margin: 10,
-  },
-  bgColor: rgbLocal => ({
+    padding: 16, paddingBottom: 50,
+  }, colorWrap: {
+    flexDirection: 'row', alignItems: 'center',
+  }, color: {
+    width: 50, height: 50, margin: 10,
+  }, bgColor: rgbLocal => ({
     backgroundColor: rgbLocal
   }),
 })
